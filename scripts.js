557eb1594e12cc2f8b6cbb7c9743991e96d228d6
Thomas Seale <ChrisSealeLaptop@Thomass-MacBook-Pro.local>
2016-06-15 12:48:49 -0500

    With extra credit too. Wait for it...

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded',function() {
 document.getElementById("add").addEventListener('click', add);
 function add(){
-    var num1 = document.getElementById('num1').value;
-    var num2 = document.getElementById('num2').value;
- 
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var DOM_img = document.createElement("img");
+    DOM_img.src = "http://www.inspirednationonline.com/wp-content/uploads/2016/04/wait-1.png";    
+    document.body.appendChild(DOM_img);
     davidsLib.add(num1, num2, function(err, result){
+        document.body.removeChild(DOM_img);    // here you can hide the indication
    	    if (err) {
             console.log ("is this thing on?");
             alert ("An error has occurred. Try again.");
@@ -16,10 +19,13 @@ function add(){
 
 document.getElementById("subtract").addEventListener('click', subtract);
 function subtract(){
-    var num1 = document.getElementById('num1').value;
-    var num2 = document.getElementById('num2').value;
-    
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var DOM_img = document.createElement("img");
+    DOM_img.src = "http://www.inspirednationonline.com/wp-content/uploads/2016/04/wait-1.png";    
+    document.body.appendChild(DOM_img);
     davidsLib.subtract(num1, num2, function(err, result) {
+        document.body.removeChild(DOM_img); 
         if (err) {
             alert ("An error has occurred. Try again.");
         } else {
@@ -32,8 +38,11 @@ document.getElementById("multiply").addEventListener('click', multiply);
 function multiply() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
-
+    var DOM_img = document.createElement("img");
+    DOM_img.src = "http://www.inspirednationonline.com/wp-content/uploads/2016/04/wait-1.png";    
+    document.body.appendChild(DOM_img);
     davidsLib.multiply(num1, num2, function(err, result) {
+        document.body.removeChild(DOM_img); 
         if (err) {
             alert ("An error has occurred. Try again.");
         } else {
@@ -46,8 +55,11 @@ document.getElementById("divide").addEventListener('click', divide);
 function divide() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
-
+    var DOM_img = document.createElement("img");
+    DOM_img.src = "http://www.inspirednationonline.com/wp-content/uploads/2016/04/wait-1.png";    
+    document.body.appendChild(DOM_img);
     davidsLib.divide(num1, num2, function(err, result) {
+        document.body.removeChild(DOM_img); 
         if (err) {
             alert("An error has occurred. Try again.");
         } else {
